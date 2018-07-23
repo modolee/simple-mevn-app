2d603c023478d2e5c06be06303c6c5c637661c77
modolee <lsi1318@gmail.com>
2018-07-23 22:45:17 +0900

    add posts page

---
+++ frontend/src/components/Posts.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="posts">
+    This file will list all the posts.
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'posts',
+  data () {
+    return {}
+  }
+}
+</script>
