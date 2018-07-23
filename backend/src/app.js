24e5ab42b67f3268b55f61b5f4c9cfb1d2cfc5e5
modolee <lsi1318@gmail.com>
2018-07-23 23:22:17 +0900

    add db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -8,6 +8,16 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/posts');
+var db = mongoose.connection;
+db.on("error", console.error.bind(console, "connection error"));
+db.once("open", function(callback) {
+    console.log("Connection Succeeded");
+});
+
+var Post = require("../models/post");
+
 app.get('/posts', (req, res) => {
   res.send(
     [{
@@ -15,6 +25,27 @@ app.get('/posts', (req, res) => {
       description: "Hi there! How ar you?"
     }]
   )
+})
+
+// Add new post
+app.post('/posts', (req, res) => {
+  var db = req.db;
+  var title = req.body.title;
+  var description = req.body.description;
+  var new_post = new Post({
+    title: title,
+    description: description
+  })
+
+  new_post.save(function (error) {
+    if (error) {
+      console.log(error)
+    }
+    res.send({
+      success: true,
+      message: 'Post saved successfully!'
+    })
+  })
 })
 
 app.listen(process.env.PORT || 8081)
