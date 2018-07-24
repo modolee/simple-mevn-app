8a31248f742b7f11341488d08a114d39f8a9118b
modolee <lsi1318@gmail.com>
2018-07-24 23:36:30 +0900

    오타 수정

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
 import Posts from '@/components/Posts'
 import NewPost from '@/components/NewPost'
-import EditPost from '@/components.EditPost'
+import EditPost from '@/components/EditPost'
 
 Vue.use(Router)
 
